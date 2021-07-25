c74a55b694895f86382fd32705312d3ed6e5e855
willy08wu07 <willy08wu07@users.noreply.github.com>
2021-07-25 15:34:20 +0800

    Add main router/controller

---
+++ public/index.php
@@ -0,0 +1,36 @@
+<?php
+
+include_once '../DatabaseConfig.php';
+include_once '../StandardTemplateView.php';
+include_once '../LoginFormView.php';
+include_once '../DashboardView.php';
+include_once '../AuthService.php';
+
+// 初始化
+session_start();
+$config = new DatabaseConfig();
+$connection = $config->newPdo();
+$authService = new AuthService();
+
+// 檢查使用者是否要求登入
+if ($authService->isLoggingIn()) {
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
+    $authService->logInOrRegister($connection, $username, $password);
+    return;
+}
+
+// 檢查使用者是否要求登出
+if ($authService->isLoggingOut()) {
+    $authService->logOut();
+    return;
+}
+
+// 檢查是否已登入
+if ($authService->isLoggedIn()) {
+    $authService->outputDashboard();
+    return;
+}
+
+// 不符合任何上述條件，那就是首次進入網站
+$authService->outputHomepage();
